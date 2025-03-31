5b0bec7f34d715159c0423918d31cc6c7fef6254
David Muñoz <davidmunozmoreno2@gmail.com>
2025-03-31 11:07:44 -0500

    feat: finished.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,38 @@
-import { useState } from 'react'
+import './Home.css'
 export const Home = () => {
-    const [usuario, setName] = useState('Usuario')
-    const [user, setUser] = useState('')
-    const addName = () => {
-        setName(user)
-    }
-    const handleChange = (e) => {
-        setUser(e.target.value)
-    }
     return (
-        <div>
-            <h1>Bienvedo {usuario}</h1>
-            <input type="text" onChange={handleChange} />
-            <button  onClick={addName}>Cambiar nombre</button>
+       <>
+       <main>
+        <div className="container">
+            <h1>To-Do List</h1>
+            <div className="add-task">
+                <input type="text" className="task-input" placeholder="Hacer ejercicio"/>
+                <input type="text" className="description-input" placeholder="Descripción"/>
+                <button id="add-button">Add Task</button>
+            </div>
+            <div class="filter">
+                <button className="filter-button active" data-filter="all">All</button>
+                <button className="filter-button" data-filter="pending">Pending</button>
+                <button className="filter-button" data-filter="completed">Completed</button>
+            </div>
+            <div className="task-list">
+                <div className="task-item" data-status="pending">
+                    <input type="checkbox" className="task-checkbox" />
+                    <div className="task-content">
+                        <div className="task-title">Barrer</div>
+                        <div className="task-description">Realizar aseo del lugar</div>
+                    </div>
+                </div>
+                <div className="task-item" data-status="pending">
+                    <input type="checkbox" className="task-checkbox"/>
+                    <div className="task-content">
+                        <div className="task-title">Estudiar para examen</div>
+                        <div className="task-description">Preparación bimestral</div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </main>
+    </>
     )
 }
